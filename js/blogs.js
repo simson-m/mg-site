31c497744e7390195331eb730dba15352e74f4a2
PUSHPARAJ19 <pushparajgprs169@gmail.com>
2025-09-14 10:27:16 +0530

    updated blogs

---
+++ js/blogs.js
@@ -0,0 +1,184 @@
+const data = [
+    {
+        title: "Harnessing AI: Transforming Tech Companies for the Future",
+        date: "July 21, 2025",
+        description1: "Discover how AI is reshaping the tech landscape by boosting efficiency, enabling smarter decision-making, and fostering innovation. Learn key strategies to integrate AI into your tech company for sustainable growth.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs1.png"
+    },
+    {
+        title: "AI Unleashed: Shaping the Next Generation of Tech Companies",
+        date: "July 22, 2025",
+        description1: "Explore how artificial intelligence is revolutionizing product development and operational agility across tech companies. Unlock practical insights into leveraging AI for competitive advantage, scalability, and long-term innovation.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs2.png"
+    },
+    {
+        title: "Redefining Tech: AI as the Catalyst for Disruption",
+        date: "July 23, 2025",
+        description1: "Delve into the transformative role of artificial intelligence in driving disruption within the tech sector. Gain actionable guidance on how tech companies can adopt AI to optimize workflows, accelerate innovation cycles, and set new standards for industry excellence.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs3.png"
+    },
+    {
+        title: "Harnessing AI: Transforming Tech Companies for the Future",
+        date: "July 24, 2025",
+        description1: "Discover how AI is reshaping the tech landscape by boosting efficiency, enabling smarter decision-making, and fostering innovation. Learn key strategies to integrate AI into your tech company for sustainable growth.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs4.png"
+    },
+    {
+        title: "Harnessing AI: Transforming Tech Companies for the Future",
+        date: "July 24, 2025",
+        description1: "Discover how AI is reshaping the tech landscape by boosting efficiency, enabling smarter decision-making, and fostering innovation. Learn key strategies to integrate AI into your tech company for sustainable growth.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs5.png"
+    },
+    {
+        title: "Harnessing AI: Transforming Tech Companies for the Future",
+        date: "July 24, 2025",
+        description1: "Discover how AI is reshaping the tech landscape by boosting efficiency, enabling smarter decision-making, and fostering innovation. Learn key strategies to integrate AI into your tech company for sustainable growth.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs6.png"
+    },
+    {
+        title: "AI Unleashed: Shaping the Next Generation of Tech Companies",
+        date: "July 22, 2025",
+        description1: "Explore how artificial intelligence is revolutionizing product development and operational agility across tech companies. Unlock practical insights into leveraging AI for competitive advantage, scalability, and long-term innovation.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs2.png"
+    },
+    {
+        title: "Redefining Tech: AI as the Catalyst for Disruption",
+        date: "July 23, 2025",
+        description1: "Delve into the transformative role of artificial intelligence in driving disruption within the tech sector. Gain actionable guidance on how tech companies can adopt AI to optimize workflows, accelerate innovation cycles, and set new standards for industry excellence.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs3.png"
+    },
+    {
+        title: "Harnessing AI: Transforming Tech Companies for the Future",
+        date: "July 24, 2025",
+        description1: "Discover how AI is reshaping the tech landscape by boosting efficiency, enabling smarter decision-making, and fostering innovation. Learn key strategies to integrate AI into your tech company for sustainable growth.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs4.png"
+    },
+    {
+        title: "Harnessing AI: Transforming Tech Companies for the Future",
+        date: "July 24, 2025",
+        description1: "Discover how AI is reshaping the tech landscape by boosting efficiency, enabling smarter decision-making, and fostering innovation. Learn key strategies to integrate AI into your tech company for sustainable growth.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs5.png"
+    },
+    {
+        title: "Harnessing AI: Transforming Tech Companies for the Future",
+        date: "July 24, 2025",
+        description1: "Discover how AI is reshaping the tech landscape by boosting efficiency, enabling smarter decision-making, and fostering innovation. Learn key strategies to integrate AI into your tech company for sustainable growth.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs6.png"
+    },
+    {
+        title: "Harnessing AI: Transforming Tech Companies for the Future",
+        date: "July 24, 2025",
+        description1: "Discover how AI is reshaping the tech landscape by boosting efficiency, enabling smarter decision-making, and fostering innovation. Learn key strategies to integrate AI into your tech company for sustainable growth.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs5.png"
+    },
+    {
+        title: "Harnessing AI: Transforming Tech Companies for the Future",
+        date: "July 24, 2025",
+        description1: "Discover how AI is reshaping the tech landscape by boosting efficiency, enabling smarter decision-making, and fostering innovation. Learn key strategies to integrate AI into your tech company for sustainable growth.",
+        description2: "Artificial intelligence (AI) is fundamentally reshaping the technology landscape. Tech companies are leveraging AI to innovate faster, streamline complex operations, and create personalized experiences. As AI continues to advance, it is becoming central to defining how tech companies compete, deliver value, and adapt to future market demands.",
+        image: "Blogs6.png"
+    }
+    // Add more objects here
+];
+
+const itemsPerPage = 6;
+let currentPage = 1;
+
+function renderContent() {
+    const contentEl = document.getElementById("content");
+    contentEl.innerHTML = "";
+
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const items = data.slice(start, end);
+
+    let i = 0, bgClass;
+    items.forEach(item => {
+        if (i % 2 == 1) {
+            bgClass = "bg-[#F7FBFF]"
+        }
+        else {
+            bgClass = "bg-white"
+        }
+        contentEl.innerHTML += `
+      <div class="${bgClass}  p-6 flex flex-col md:flex-row gap-6 py-[60px] px-[100px]">
+        <img src="assets/${item.image}" class="w-full md:w-1/4 rounded" alt="${item.title}">
+        <div>
+          <p class="text-sm text-[#1783F7] mb-2">${item.date}</p>
+          <h2 class="text-2xl font-semibold text-[#055CC0] mb-2">${item.title}</h2>
+          <p class="text-[#667085] mb-4">${item.description1}</p>
+          <p class="text-[#667085] mb-4">${item.description2}</p>
+          <a href="BlogDetail.html" class="text-[#1783F7] underline inline-flex items-center gap-1">
+          Read More
+          <img src="/assets/home/navarrow.svg" alt="arrow" class="w-6  h-6">
+          </a>
+        </div>
+      </div>
+    `;
+        i++;
+    });
+}
+
+function renderPagination() {
+    const paginationEl = document.getElementById("pagination");
+    paginationEl.innerHTML = "";
+
+    const totalPages = Math.ceil(data.length / itemsPerPage);
+
+    // Previous button
+    paginationEl.innerHTML += `<div>
+    <button ${currentPage === 1 ? 'disabled' : ''}
+      onclick="goToPage(${currentPage - 1})"
+      style="border: 2px solid #E8F0FC"
+      class="p-2 rounded  text-bold text-[#0764E3]  hover:bg-gray-100 ${currentPage === 1 ? 'opacity-50 cursor-not-allowed' : ''}">
+      <img src="assets/LeftArrowIcon.svg" alt="arrow">
+    </button></div>
+  `;
+
+    // Page numbers
+    let content = "";
+    for (let i = 1; i <= totalPages; i++) {
+        content += `
+      <button onclick="goToPage(${i})"
+        class="px-3 py-1 rounded-md border ${currentPage === i ? 'bg-[#1783F7] text-white' : 'text-[#0764E3] hover:bg-gray-100'}">
+        ${i}
+      </button>
+    `;
+    }
+    paginationEl.innerHTML += `<div>${content}</div>`
+
+    // Next button
+    paginationEl.innerHTML += `
+    <div><button ${currentPage === totalPages ? 'disabled' : ''}
+      onclick="goToPage(${currentPage + 1})"
+      style="border: 2px solid #E8F0FC"
+      class="p-2 rounded  text-[#0764E3] hover:bg-gray-100 ${currentPage === totalPages ? 'opacity-50 cursor-not-allowed' : ''}">
+      <img src="assets/LeftArrowIcon.svg" alt="arrow" style="transform: rotate(180deg);">
+    </button></div>
+  `;
+}
+
+function goToPage(page) {
+    const totalPages = Math.ceil(data.length / itemsPerPage);
+    if (page < 1 || page > totalPages) return;
+    currentPage = page;
+    renderContent();
+    renderPagination();
+}
+
+window.onload = () => {
+    renderContent();
+    renderPagination();
+};
